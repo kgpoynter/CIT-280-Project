2e88c8f84484566c3cef36bc538db9754b404e37
Gkabacin <gkabacin@gmail.com>
2015-10-31 20:52:44 -0400

    I made a mostly complete GUI. I hope you like it.

--- source/edu/oakland/quizwiz/production/Database.java
+++ source/edu/oakland/quizwiz/production/Database.java
@@ -3,6 +3,7 @@ package edu.oakland.quizwiz.production;
 import edu.oakland.quizwiz.production.*;
 import java.util.*;
 import java.io.*;
+import javax.swing.DefaultListModel;
 
 /**
 * This class defines an object that holds all of the Quiz objects in memory
@@ -14,48 +15,19 @@ public class Database {
 	private ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
 	
 	public void add_quiz(Quiz qz) {
-		quizzes.add(qz);
+                quizzes.add(qz);
 	}
+        
+        public Quiz get_quiz(int index) {
+            return quizzes.get(index);
+        }
 
-	public void list_quizzes() {
+	public DefaultListModel list_quizzes() {
 		Iterator<Quiz> it = quizzes.iterator();
+                DefaultListModel<String> quiz_list = new DefaultListModel<String>();
 		while(it.hasNext()) {
-			System.out.println(it.next().get_title());
+			quiz_list.addElement(it.next().get_title());
 		}
-	}
-
-	public void export_quiz(int index) throws IOException {
-		Quiz quiz = quizzes.get(index);
-		Question q;
-		String filename = "quiz/";
-		filename += quiz.get_title();
-		filename += ".txt";
-		PrintWriter writer = new PrintWriter(filename, "UTF-8");
-		writer.println(quiz.get_title());
-		writer.println(quiz.get_num_questions());
-		writer.println();
-		int count = 0;
-		while (count < quiz.get_num_questions()) {
-			q = quiz.get_question(count);
-			writer.println(q.get_question_type());
-			if (q.get_question_type().equals("MC")) {
-				ArrayList<String> selections = q.get_selections();
-				int sel_num = selections.size();
-				int sel_count = 0;
-				writer.println(sel_num);
-				writer.println(q.get_question_text());
-				while (sel_count < sel_num) {
-					writer.println(selections.get(sel_count));
-					sel_count = sel_count + 1;
-				}
-				writer.println(q.get_answer_index());
-			} else {
-				writer.println(q.get_question_text());
-				writer.println(q.get_answer_string());	
-			}
-			writer.println();
-			count = count + 1;
-		}
-		writer.close();
+                return quiz_list;
 	}
 }
--- source/edu/oakland/quizwiz/production/Quiz.java
+++ source/edu/oakland/quizwiz/production/Quiz.java
@@ -1,6 +1,8 @@
 package edu.oakland.quizwiz.production;
 
 import edu.oakland.quizwiz.production.Question;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
@@ -31,8 +33,46 @@ public class Quiz {
 	public String get_title() {
 		return title;
 	}
+        
+        public void set_title(String _title) {
+                title = _title;
+        }
 
 	public int get_num_questions() {
 		return num_questions;
+	}
+        
+        public void export_quiz() throws IOException {
+		Question q;
+		String filename = "quiz/";
+		filename += get_title();
+		filename += ".txt";
+		PrintWriter writer = new PrintWriter(filename, "UTF-8");
+		writer.println(get_title());
+		writer.println(get_num_questions());
+		writer.println();
+		int count = 0;
+		while (count < get_num_questions()) {
+			q = get_question(count);
+			writer.println(q.get_question_type());
+			if (q.get_question_type().equals("MC")) {
+				ArrayList<String> selections = q.get_selections();
+				int sel_num = selections.size();
+				int sel_count = 0;
+				writer.println(sel_num);
+				writer.println(q.get_question_text());
+				while (sel_count < sel_num) {
+					writer.println(selections.get(sel_count));
+					sel_count = sel_count + 1;
+				}
+				writer.println(q.get_answer_index());
+			} else {
+				writer.println(q.get_question_text());
+				writer.println(q.get_answer_string());	
+			}
+			writer.println();
+			count = count + 1;
+		}
+		writer.close();
 	}
 }
